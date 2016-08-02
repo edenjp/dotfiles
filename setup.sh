4690169caf5aa4c4398b68376a3919d3b325a84f
Fabio Perrella <fabio.perrella@locaweb.com.br>
2016-08-02 19:48:17 -0300

    include bashrc_includes to bashrc

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 set -e
+export bashrc_file=~/.bashrc
 
 dotfiles=$(ls -A | egrep '^\.' | grep -v ^.git$)
 for i in $dotfiles
 do
   ln -s $PWD/$i ~/$i || echo "did not create symlink for $i because file ~/$i already exists"
 done
+
+grep bashrc_includes $bashrc_file || echo "source .bashrc_includes" >> $bashrc_file
+
+source $bashrc_file
 
 echo "done!"
