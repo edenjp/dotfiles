37053c1cc9dd2cc3ab547cdec2383b99ca32d024
Fabio Perrella <fabio.perrella@locaweb.com.br>
2016-08-02 20:16:56 -0300

    moved dotfiles to dir dotfiles

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 
 export bashrc_file=~/.bashrc
 
-dotfiles=$(ls -A | egrep '^\.' | grep -v ^.git$)
+dotfiles=$(ls -A dotfiles| egrep '^\.')
 for i in $dotfiles
 do
-  ln -sf $PWD/$i ~/$i || echo "did not create symlink for $i because file ~/$i already exists"
+  ln -sf $PWD/dotfiles/$i ~/$i || echo "did not create symlink for $i because file ~/$i already exists"
 done
 
-grep bashrc_includes $bashrc_file || echo "source .bashrc_includes" >> $bashrc_file
+grep bashrc_includes $bashrc_file || echo "source ~/.bashrc_includes" >> $bashrc_file
 
 source $bashrc_file
 
